920b050bd170978b03270640c12cb046e97b2bf7
jerryliang <jerryliang@jerryliang.top>
2022-12-16 17:57:04 +0800

    完成发送微信消息

--- api/sql.py
+++ api/sql.py
@@ -20,10 +20,10 @@ from sqlalchemy.ext.declarative import declarative_base
 init_data = json.loads(open('conf/sql.json', 'r', encoding='utf-8').read())
 # 连接数据库
 class DB:
-    def __init__(self, db):
+    def __init__(self):
         self.engine = create_engine(f'mysql+pymysql://{init_data["name"]}:{init_data["password"]}@localhost:3306/comprehensive_push',max_overflow=2,pool_pre_ping=True,pool_recycle=1600,pool_use_lifo=True)
-        DBsession = sessionmaker(bind=self.engine)
-        self.db = DBsession()
+        
+        
     
     def init(self):
         Base.metadata.create_all(self.engine)
@@ -37,6 +37,11 @@ class token(Base):
     id = Column(Integer, primary_key=True)
     access_token = Column(String(255))
     expiration_time = Column(DateTime)
+
+    def __init__(self,id,access_token,expiration_time):
+        self.id = id
+        self.access_token = access_token
+        self.expiration_time = expiration_time
 
 # 生成数据库
 db = DB().init()
