e6533fb131c74a68562b44c6608dfcd873be4f1b
jerryliang <jerryliang@jerryliang.top>
2022-12-16 16:38:45 +0800

    微信token数据库构建

--- api/sql.py
+++ api/sql.py
@@ -1,2 +1,42 @@
 import json
-import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (
+    Column,
+    create_engine,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+    ForeignKey,
+    Float,
+    Text,
+    LargeBinary,
+    DECIMAL,
+    BigInteger,
+)
+from sqlalchemy.ext.declarative import declarative_base
+
+init_data = json.loads(open('conf/sql.json', 'r', encoding='utf-8').read())
+# 连接数据库
+class DB:
+    def __init__(self, db):
+        self.engine = create_engine(f'mysql+pymysql://{init_data["name"]}:{init_data["password"]}@localhost:3306/comprehensive_push',max_overflow=2,pool_pre_ping=True,pool_recycle=1600,pool_use_lifo=True)
+        DBsession = sessionmaker(bind=self.engine)
+        self.db = DBsession()
+    
+    def init(self):
+        Base.metadata.create_all(self.engine)
+        return self.db
+    
+
+# 数据库
+Base = declarative_base()
+class token(Base):
+    __tablename__ = 'token'
+    id = Column(Integer, primary_key=True)
+    access_token = Column(String(255))
+    expiration_time = Column(DateTime)
+
+# 生成数据库
+db = DB().init()
