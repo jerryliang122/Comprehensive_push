a3975ec2106f8fd1ef6b9ec6bf53544b5665f11e
jerryliang <jerryliang@jerryliang.top>
2022-12-29 15:22:28 +0800

    Temporary storage

--- api/action_response.py
+++ api/action_response.py
@@ -1,1 +1,74 @@
 from api.wechat import wecat_token
+import paramiko
+import json5
+from api.sql import DB
+from sqlalchemy.orm import sessionmaker
+
+# 路由器IPSET
+class ipset(DB):
+    # 初始化
+    def __init__(self):
+        self.config = json5.load(open("conf/ssh.json", "r", encoding="utf-8"))
+        super().__init__()
+        self.session = sessionmaker(bind=self.engine)
+
+    # 查询ip是否存在
+    def query_ip(self, type, ip):
+        result = self.session.query(ipset).filter(ipset.ip == ip).count()
+        if result == 0:
+            return False
+        result = self.session.query(ipset).filter(ipset.ip == ip).first()
+        if result.ip == ip:
+            return True
+
+    # 添加ip进入数据库
+    def add_ip(self, type, ip):
+        try:
+            if self.query_ip(type, ip) == True:
+                return False
+            self.session.add(ipset(type, ip))
+            self.session.commit()
+            return True
+        except:
+            return False
+
+    # 删除ip
+    def del_ip(self, type, ip):
+        try:
+            self.session.query(ipset).filter(ipset.type == type, ipset.ip == ip).delete()
+            self.session.commit()
+            return True
+        except:
+            return False
+
+    # 回复
+    def reply(self, FromUserName, text):
+        # 调用微信发送插件
+        wx = wecat_token()
+        # 发送消息
+        msg = wx.send_msg_text(text, FromUserName)
+
+    # 向路由器添加IP
+    def add_ip_to_router(self, type, ip, FromUserName):
+        # 建立ssh连接
+        router = paramiko.SSHClient()
+        # 允许连接不在know_hosts文件中的主机
+        router.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # 调用connect方法连接服务器
+        router.connect(
+            hostname=self.config["ssh-1"]["host"],
+            port=self.config["ssh-1"]["port"],
+            username=self.config["ssh-1"]["user"],
+            password=self.config["ssh-1"]["password"],
+        )
+        # 写入数据库
+        if self.add_ip(type, ip) == True:
+            # 回复IP已添加，无需重复添加
+            self.reply(FromUserName, "IP已添加，无需重复添加")
+            return None
+        # 执行命令
+        stdin, stdout, stderr = router.exec_command(f"ipset add {type} {ip}")
+        # 获取命令结果
+        result = stdout.read().decode()
+        # 关闭连接
+        router.close()
--- api/sql.py
+++ api/sql.py
@@ -1,6 +1,6 @@
 import json
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
 from sqlalchemy import (
     Column,
     create_engine,
@@ -17,41 +17,50 @@ from sqlalchemy import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 
-init_data = json.loads(open('conf/sql.json', 'r', encoding='utf-8').read())
+init_data = json.loads(open("conf/sql.json", "r", encoding="utf-8").read())
 # 连接数据库
 class DB:
     def __init__(self):
-        self.engine = create_engine(f'mysql+pymysql://{init_data["name"]}:{init_data["password"]}@localhost:3306/comprehensive_push',max_overflow=2,pool_pre_ping=True,pool_recycle=1600,pool_use_lifo=True)
-        
-        
-    
+        self.engine = create_engine(
+            f'mysql+pymysql://{init_data["name"]}:{init_data["password"]}@localhost:3306/comprehensive_push',
+            max_overflow=2,
+            pool_pre_ping=True,
+            pool_recycle=1600,
+            pool_use_lifo=True,
+        )
+
     def init(self):
         Base.metadata.create_all(self.engine)
         return self.db
-    
+
 
 # 数据库
 Base = declarative_base()
+
+
 class token(Base):
-    __tablename__ = 'token'
+    __tablename__ = "token"
     id = Column(Integer, primary_key=True)
     access_token = Column(String(255))
     expiration_time = Column(DateTime)
 
-    def __init__(self,id,access_token,expiration_time):
+    def __init__(self, id, access_token, expiration_time):
         self.id = id
         self.access_token = access_token
         self.expiration_time = expiration_time
 
+
 class ipset(Base):
-    __tablename__ = 'ipset'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = "ipset"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(Text)
     ip = Column(Text)
-    
-    def __init__(self,id,type,ip):
+
+    def __init__(self, id, type, ip):
         self.id = id
         self.type = type
         self.ip = ip
+
+
 # 生成数据库
 db = DB().init()
