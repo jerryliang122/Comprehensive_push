0d94eafdbfaf4297f8e9cd700ad024a66feecf14
jerryliang <jerryliang@jerryliang.top>
2023-01-04 16:27:15 +0800

    1

--- web/app.py
+++ web/app.py
@@ -1,8 +1,8 @@
 from sanic import Sanic
-from web.wechat import wechat
+from .web import web
 
 
 def main():
     app = Sanic()
-    app.blueprint(wechat)
+    app.blueprint(web)
     app.run(host="127.0.0.1", port=8000)
